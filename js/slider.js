c02e1f01d3d03e9b2a5ab3b1caddbed2d0e20079
Feilipu <philippe.bihin@gmail.com>
2019-04-29 15:04:37 +0200

    Ajout de commentaires

--- js/slider.js
+++ js/slider.js
@@ -1,49 +1,53 @@
-// Slider
 const Slider = {
 	slideIndex: 0,
 	timer: null,
 
 	init: function(mySlides, prev, next, start, stop){
-		this.slide = mySlides;
+		this.slide = mySlides; // this.slide.length = 4 pour les 4 images du diaporama
 		this.btnPrev = prev; // btn = button
 		this.btnNext = next;
 		this.btnStart = start;
 		this.btnStop = stop;
 		this.previous();
 		this.next();
-		this.automatic(); // this.showSlide() in this method, displays images.
-		this.playStop();
+		this.automatic(); // défilement du diaporama 
+		this.playStop(); // boutons play et stop
 	},
 
+	// retrait de 1 à l'indice de l'objet this.slide
 	indexMinus: function(){
 		this.slideIndex -= 1;
 		this.showSlide();
 	},
 
+	// Ajout de 1 à l'indice de l'objet this.slide
 	indexPlus: function(){
 		this.slideIndex += 1;
 		this.showSlide();
 	},
 
+	// Affiche d'une image selon l'indice
 	showSlide: function(){
-		this.slide.hide();
+		this.slide.hide(); // tout le diaporama est caché
 
-		if (this.slideIndex < 0) {
+		if (this.slideIndex < 0) { // si l'indice < 0 affiche la dernière image
 			this.slideIndex = this.slide.length - 1;
 		}
 
-		if (this.slideIndex > this.slide.length - 1) {
+		if (this.slideIndex > this.slide.length - 1) { // si l'indice est > 4 affiche la première image
 			this.slideIndex = 0;
 		}
 
-		this.slide[this.slideIndex].style.display = "block";
+		this.slide[this.slideIndex].style.display = "block"; // affiche une image avec un indice précis
 	},
 
 	previous: function(){
+		// bouton image précédente
 		this.btnPrev.click(function(){
-			this.indexMinus();
+			this.indexMinus(); // retire 1 à l'indice 
 		}.bind(this));
 
+		// flèche gauche du clavier
 		$("body").keydown(function(e){
 			if (e.keyCode === 37) {
 				this.indexMinus();
@@ -52,10 +56,12 @@ const Slider = {
 	},
 
 	next: function(){
+		// bouton image suivante
 		this.btnNext.click(function(){
 			this.indexPlus();
 		}.bind(this));
 
+		// flèche droite du clavier
 		$("body").keydown(function(e){
 			if (e.keyCode === 39) {
 				this.indexPlus();
@@ -63,18 +69,21 @@ const Slider = {
 		}.bind(this));
 	},
 
+	// défilement du diaporama toutes les 5 secondes
 	automatic: function(){
 		this.showSlide();
 		this.timer = setInterval(function(){
-			this.indexPlus();
+			this.indexPlus(); // ajout de 1 à l'indice toutes les 5s
 		}.bind(this), 5000);
 	},
 
 	playStop: function(){
+		// mise en route du diaporama lors du clique sur le bouton play
 		this.btnStart.click(function(){
 			this.automatic();
 		}.bind(this));
 
+		// arrêt du diaporama lors du clique sur le bouton stop
 		this.btnStop.click(function(){
 			clearInterval(this.timer);
 		}.bind(this));
