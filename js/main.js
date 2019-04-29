88feacfe842eb21984c4689fcf9928aca19166a7
Feilipu <philippe.bihin@gmail.com>
2019-04-29 15:06:12 +0200

    essai de modification annulé

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){ // Tout se lance une fois la page chargée
-// slider
-let slide = Object.create(Slider);
-slide.init($(".mySlides"), $(".prev"), $(".next"), $(".play"), $(".stop"));
+	// slider
+	let slide = Object.create(Slider);
+	slide.init($(".mySlides"), $(".prev"), $(".next"), $(".play"), $(".stop"));
 
-// Town's map
-let town = Object.create(Map);
-town.init("nantes", 47.217, -1.553, 12);
+	// Town's map
+	let town = Object.create(Map);
+	town.init("nantes", 47.217, -1.553, 12);
 });
